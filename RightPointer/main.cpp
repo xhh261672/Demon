bc0c05ca532f3d04814a52412b4ce3814de2a90e
xhh261672 <563249383@qq.com>
2014-11-05 00:46:03 +0800

    search insert

--- RightPointer/main.cpp
+++ RightPointer/main.cpp
@@ -47,3 +47,19 @@ public:
         }
     }
 };
+/****************standard solution********************/
+
+void connect(TreeLinkNode *root) {
+    if (root == NULL) return;
+    TreeLinkNode *pre = root;
+    TreeLinkNode *cur = NULL;
+    while(pre->left) {
+        cur = pre;
+        while(cur) {
+            cur->left->next = cur->right;
+            if(cur->next) cur->right->next = cur->next->left;
+            cur = cur->next;
+        }
+        pre = pre->left;
+    }
+}
---
+++ searchInsert/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+
+    }
+};
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
