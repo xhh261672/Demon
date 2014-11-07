fe48f653576f58b2e6e0961306aa756b79d49379
xhh261672 <563249383@qq.com>
2014-11-07 15:28:06 +0800

    18/154

--- maxSubarray/main.cpp
+++ maxSubarray/main.cpp
@@ -4,11 +4,25 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
+        int result = A[0];
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += A[i];
+            if (sum > result)
+                result = sum;
+            if (sum < 0)
+                sum = 0;
 
+        }
+
+        return result;
     }
+
 };
 int main()
 {
-    cout << "Hello world!" << endl;
+    Solution s;
+    int A[10] = {1,-2,-3,-1,4,2,1,-5,4};
+    cout<<s.maxSubArray(A, 10)<<endl;
     return 0;
 }
