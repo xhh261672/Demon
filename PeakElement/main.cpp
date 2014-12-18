23739ea1edb7ee29fe5d3800d21400a781d80048
xhh261672 <563249383@qq.com>
2014-12-18 10:23:57 +0800

    25/166

--- PeakElement/main.cpp
+++ PeakElement/main.cpp
@@ -20,6 +20,22 @@ public:
 
 //O(logN) solution
 
+class Solution2 {
+public:
+    int findPeakElement (const vector<int> &num) {
+        int left = 0, right = num.size() - 1;
+        while (left <=right){
+            if (left == right)
+                return left;
+            int mid = (left + right)/2;
+            if (num[mid] < num[mid+1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+    }
+};
+
 
 int main()
 {
