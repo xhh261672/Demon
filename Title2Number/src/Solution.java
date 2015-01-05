fac9b7d4a0c7c67654d20b2451e692b6665af600
xhh261672 <563249383@qq.com>
2015-01-06 00:14:18 +0800

    31

--- Title2Number/src/Solution.java
+++ Title2Number/src/Solution.java
@@ -1,6 +1,18 @@
 
 public class Solution {
 	 public int titleToNumber(String s) {
-		 return 0;
+		 if (s == null)
+			 return 0;
+		 int result = 0;
+		 int len = s.length();
+		 for (int i = 0; i < len; i ++){
+			 result += (s.charAt(i) - 'A' + 1) * java.lang.Math.pow(26.0, len-i-1);
+		 }
+		 return result;
+	 }
+	 
+	 public static void main(String []args){
+		 Solution solution = new Solution();
+		 System.out.print(solution.titleToNumber("AA"));
 	 }
 }
