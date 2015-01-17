410e60663e2a11043d8d0f03f4cfd3e38240e353
xhh261672 <563249383@qq.com>
2015-01-18 01:07:52 +0800

    33/175

--- ReverseWords/src/Solution.java
+++ ReverseWords/src/Solution.java
@@ -3,6 +3,27 @@
  */
 public class Solution {
     public String reverseWords(String s) {
+        if (s == null)
+            return null;
+        if (s.equals(""))
+            return "";
+        s = s.trim();
+        String []ss = s.split("\\s+");
+        //System.out.println(ss[0]);
+        StringBuffer sbuffer = new StringBuffer();
+        for (int i = ss.length -1 ; i >=0; i--){
+            sbuffer.append(ss[i]);
+            if (i != 0)
+                sbuffer.append(" ");
+        }
+        s = sbuffer.toString();
+        //System.out.print(s);
         return s;
+    }
+
+    public static void main(String []args){
+        String s = " 1";
+        Solution solution = new Solution();
+        System.out.print(solution.reverseWords(s));
     }
 }
