8fa8db0d0503b4e3a52cc86671c11d00a107bd25
xhh261672 <563249383@qq.com>
2015-02-02 11:41:15 +0800

    43

--- Subsets/src/Solution.java
+++ Subsets/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -5,6 +7,17 @@ import java.util.List;
  */
 public class Solution {
     public List<List<Integer>> subsets(int[] S) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        Arrays.sort(S);
 
+        for (int i = 0; i < 1<<(S.length); i++){
+            List<Integer> result = new ArrayList<Integer>();
+            for (int j = 0; j < S.length; j++){
+                if ((i&(1<<j))!= 0) result.add(S[j]);
+            }
+            results.add(result);
+        }
+
+        return results;
     }
 }
