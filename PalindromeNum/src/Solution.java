3100ca11df92b5d9fb149ebadcc8defbaba7e552
xhh261672 <563249383@qq.com>
2015-02-03 09:52:32 +0800

    45

---
+++ PalindromeNum/src/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 2015/2/3.
+ */
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        int l = 0, r = 0, len = 0, t = x;
+        while (t != 0){
+            t = t/10;
+            len ++;
+        }
+//        System.out.println(len);
+        if (len == 1) return true;
+        if (len == 2 && x % 11 == 0) return true;
+
+        while (true){
+            if (len == 1) return true;
+            if (len == 2 && x % 11 == 0) return true;
+
+            int mag = (int) Math.pow(10.0, len -1);
+            l = x / mag;
+            x %= mag;
+            len --;
+//            mag = (int) Math.pow(10.0, len -1);
+            r = x % 10;
+            x /= 10;
+            len --;
+            if (l != r) return false;
+        }
+    }
+
+    public static void main(String []args){
+        Solution solution = new Solution();
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(solution.isPalindrome(-2147447412));
+    }
+
+}
