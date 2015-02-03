72a8a60453edb677dc79d1f7664925c2be8e0315
xhh261672 <563249383@qq.com>
2015-02-03 20:56:04 +0800

    48

---
+++ No11/src/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2015/2/3.
+ */
+public class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length -1;
+        int max = 0;
+
+        while (i < j){
+            max = Math.max(max, (j - i)* Math.min(height[i], height[j]));
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+        return max;
+
+    }
+}
