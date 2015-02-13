bc436e7037255606cd445b3ae3c34c89ea0486d3
xhh261672 <563249383@qq.com>
2015-02-13 09:49:57 +0800

    70

--- LevelTranversal/src/Solution.java
+++ LevelTranversal/src/Solution.java
@@ -24,4 +24,26 @@ public class Solution {
 
         return re;
     }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> re = new ArrayList<List<Integer>>();
+        if (root == null) return re;
+
+        Queue <TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int level = queue.size();
+            List<Integer> temp = new ArrayList<Integer>();
+            for (int i = 0; i < level; i++){
+                TreeNode cur = queue.poll();
+                temp.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            re.add(temp);
+        }
+
+        Collections.reverse(re);
+        return re;
+    }
 }
