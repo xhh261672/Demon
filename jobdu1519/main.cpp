769b954590983ec50611a60854013351d0d9f41d
xhh261672 <563249383@qq.com>
2015-05-03 17:32:43 +0800

    1519

--- jobdu1519/main.cpp
+++ jobdu1519/main.cpp
@@ -16,6 +16,42 @@ void print(ListNode *head){
     cout<<head->val<<endl;
 }
 
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (l1 && l2){
+            ListNode *start = NULL;
+            if (l1->val < l2->val){
+                start = l1;
+                l1 = l1->next;
+            }
+            else{
+                start = l2;
+                l2 = l2->next;
+            }
+            ListNode *current = start;
+            while (l1 && l2){
+                if (l1->val > l2->val){
+                    current->next = l2;
+                    l2 = l2->next;
+                }
+                else{
+                    current->next = l1;
+                    l1 = l1->next;
+                }
+                current = current->next;
+            }
+            if (l1)
+                current->next = l1;
+            else
+                current->next = l2;
+            return start;
+        }
+        else if (l1){
+            return l1;
+        }
+        else
+            return l2;
+    }
+
 int main()
 {
     int m = 0, n = 0;
@@ -42,22 +78,24 @@ int main()
         if (n == 0) {print(h2->next);continue;}
         if (m == 0) {print(h1->next);continue;}
 
-        cur = h1->next;
-        ListNode *cur2 = h2->next;
+//        cur = h1->next;
+//        ListNode *cur2 = h2->next;
+//
+//        while (cur && cur2){
+//            if (cur->val <= cur2->val){
+//                ListNode *temp = cur->next;
+//                cur->next = cur2;
+//                cur = temp;
+//            }else{
+//                ListNode *temp = cur2->next;
+//                cur2->next = cur;
+//                cur2 = temp;
+//            }
+//        }
+//        if (h1->next->val <= h2->next->val) print(h1->next);
+//        else print(h2->next);
+        print(mergeTwoLists(h1->next, h2->next));
 
-        while (cur && cur2){
-            if (cur->val <= cur2->val){
-                ListNode *temp = cur->next;
-                cur->next = cur2;
-                cur = temp;
-            }else{
-                ListNode *temp = cur2->next;
-                cur2->next = cur;
-                cur2 = temp;
-            }
-        }
-        if (h1->next->val <= h2->next->val) print(h1->next);
-        else print(h2->next);
     }
     return 0;
 }
