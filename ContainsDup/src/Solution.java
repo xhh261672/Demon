bf6f44b34752456888aa559c05b4185e7a14fb48
xhh261672 <563249383@qq.com>
2015-05-30 20:24:59 +0800

    108

---
+++ ContainsDup/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2015/5/30.
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])) return true;
+            map.put(nums[i], true);
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])){
+                int j = map.get(nums[i]);
+                if (i-j <= k) return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
