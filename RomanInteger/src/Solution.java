45c9d0086bf519191cb8d50dd8b2f4584ed6ed76
xhh261672 <563249383@qq.com>
2015-06-03 11:24:48 +0800

    11

--- RomanInteger/src/Solution.java
+++ RomanInteger/src/Solution.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,10 +31,70 @@ public class Solution {
     }
 
     public String intToRoman(int num) {
+        StringBuffer sb = new StringBuffer();
+        List<Integer> list = new ArrayList<Integer>();
+        int bits = 0;
+        while (num != 0){
+            bits++;
+            list.add(num%10);
+            num /= 10;
+        }
 
+        for (int i = bits; i > 0; i--){
+            int digit = list.get(i-1);
+            if (i == 4){
+                for (int j = 0; j < digit; j++)
+                    sb.append('M');
+            }
+            else if (i == 3){
+                if (digit < 4){
+                    for (int j = 0; j < digit; j++)
+                        sb.append('C');
+                }else if (digit == 4){
+                    sb.append("CD");
+                }else if (digit == 5){
+                    sb.append("D");
+                }else if (digit < 9){
+                    sb.append("D");
+                    for (int j = 0; j < digit-5; j++)
+                        sb.append('C');
+                }else
+                    sb.append("CM");
+            }else if (i == 2){
+                if (digit < 4){
+                    for (int j = 0; j < digit; j++)
+                        sb.append('X');
+                }else if (digit == 4){
+                    sb.append("XL");
+                }else if (digit == 5){
+                    sb.append("L");
+                }else if (digit < 9){
+                    sb.append("L");
+                    for (int j = 0; j < digit-5; j++)
+                        sb.append('X');
+                }else
+                    sb.append("XC");
+            }else if (i == 1){
+                if (digit < 4){
+                    for (int j = 0; j < digit; j++)
+                        sb.append('I');
+                }else if (digit == 4){
+                    sb.append("IV");
+                }else if (digit == 5){
+                    sb.append("V");
+                }else if (digit < 9){
+                    sb.append("V");
+                    for (int j = 0; j < digit-5; j++)
+                        sb.append('I');
+                }else
+                    sb.append("IX");
+            }
+        }
+
+        return sb.toString();
     }
 
-    
+
 
     public static void main (String []args){
         Solution solution = new Solution();
